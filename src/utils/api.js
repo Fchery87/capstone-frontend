8fc418443a69614c3d8ea2ccf3d0d81293149185
Frantz Chery <frantzchery@hotmail.com>
2024-06-17 16:04:34 -0400

    added renderurl to api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL: 'https://capstone-backend-9d1u.onrender.com',
+  // baseURL: 'http://localhost:4000/api',
 });
 
 export default API;

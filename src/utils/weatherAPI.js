9f162a53dd050742620984750ef818f147211304
Frantz Chery <frantzchery@hotmail.com>
2024-06-18 15:05:12 -0400

    https

--- src/utils/weatherAPI.js
+++ src/utils/weatherAPI.js
@@ -5,7 +5,7 @@ const API_KEY = '9438aa16e9c04dcf8c7183223241806';
 export const getWeather = async (location) => {
   try {
     const response = await axios.get(
-      `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&days=8`
+      `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&days=8`
     );
     return response.data;
   } catch (error) {
